032790c3ea17c567c747a5367510b6465266de17
Izana <shubhmishra581@gmail.com>
2025-07-16 20:40:22 +0530

    Update pass or fail.c
    adding an output for non valid marks

--- pass or fail.c
+++ pass or fail.c
@@ -5,7 +5,11 @@ int main() {
     printf("Enter the marks: ");
     scanf("%d", &a);
     
-    a>=33 ? printf("You have passed!") : printf("You have failed.");
+    if (a>=0 && a<=100){
+        a>=33 ? printf("You have passed!") : printf("You have failed.");
+    } else {
+        printf("!!! Marks can only be 0-100 !!!");
+    }
 
     return 0;
 }
