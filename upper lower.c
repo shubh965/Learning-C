25b9227abf5720ae1ee52447600138bd6208294f
Izana <shubhmishra581@gmail.com>
2025-07-21 11:53:26 +0530

    Create upper lower.c

---
+++ upper lower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Enter the character: ");
+    scanf(" %c", &c);
+    
+    if (c>='a' && c<='z') {
+        printf("The character is in lower caps.\n");
+    } else if (c>='A' && c<='Z') {
+        printf("The character is in capital caps.\n");
+    } else {
+        printf("Please enter a-z or A-Z.\n");
+    }
+
+    return 0;
+}
