47e8da53bf8b62e3fc7a70b2c5e38924921b3cd9
Shubh <shubhmishra581@gmail.com>
2025-08-07 20:47:58 +0530

    Update tables.c

--- tables.c
+++ tables.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &n);
     
     for (int i=1; i<=10; i++) {
-        printf("%d X %d = %d\n", n, i, n*i);
+        printf("%d x %d = %d\n", n, i, n*i);
     }
 
     return 0;
