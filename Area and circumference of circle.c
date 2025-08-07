d7cb8bf78cae5b02091dc5dc1751d9427dcc4768
Shubh <shubhmishra581@gmail.com>
2025-08-07 20:52:52 +0530

    Update Area and circumference of circle.c

--- Area and circumference of circle.c
+++ Area and circumference of circle.c
@@ -4,6 +4,8 @@ int main() {
     float r;
     printf("Enter the radius of the circle: ");
     scanf("%f", &r);
+    
     printf("\nThe circumference of the circle is: %f \nThe area of the circle is: %f", 2*3.14159*r, 3.14159*r*r);
+    
     return 0;
 }
